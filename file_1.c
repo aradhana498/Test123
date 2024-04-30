f8294a5bcfd064beb71f1f14f19f0df236edb02c
2020wa86939@wilp.bits-pilani.ac.in <labuser@ip-172-31-9-18.ap-south-1.compute.internal>
2024-04-30 16:23:31 +0000

    adding same function from main branch

---
+++ file_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+	int num1,num2,sum1;
+	printf("enter two integers");
+	scanf("%d %d", &num1, &num2);
+	sum1 = num1 - num2;
+      
+	printf("%d + %d = %d", num1,num2,sum1);
+	return 0;
+
+
+}
+
